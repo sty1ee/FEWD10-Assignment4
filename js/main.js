4663d1fccbfdfac023e9e67bdab2873b909ab559
sty1ee <lsavage3@ON7000NN005.officeworks.internal>
2016-02-24 21:00:10 +1100

    Push #2

--- js/main.js
+++ js/main.js
@@ -8,30 +8,35 @@ $('#submit-btn').click(function(event) {
 		if (city == 'nyc' || city == 'new york city' || city == 'new york') {
 			$('body').removeClass();
 			$('body').addClass('nyc');
-			$('#city-type').reset(); 
+			$('#city-type').val('');
 
 		}
 	
 		else if (city == 'sf' || city == 'sanfransico' || city == 'sanfran') {
 			$('body').removeClass();
-			$('body').addClass('sf'); 
+			$('body').addClass('sf');
+			$('#city-type').val(''); 
 		}
 
 		else if (city == 'LA' || city == 'Losangeles' || city == 'la') {
 			$('body').removeClass();
-			$('body').addClass('la'); 
+			$('body').addClass('la');
+			$('#city-type').val(''); 
 		}	
 
 		else if (city == 'Austin' || city == 'austin' || city == 'AUSTIN') {
 			$('body').removeClass();
-			$('body').addClass('austin'); 
+			$('body').addClass('austin');
+			$('#city-type').val(''); 
 
 		}
 
 		else if (city == 'syd' || city == 'sydney' || city == 'Sydney') {
 			$('body').removeClass();
-			$('body').addClass('sydney'); 
+			$('body').addClass('sydney');
+			$('#city-type').val(''); 
 		}
+
 
 	});
 
